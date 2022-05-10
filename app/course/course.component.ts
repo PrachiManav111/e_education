0dbdac37b123360c45d783099844689506e0b955
Prachi Manav <105165863+PrachiManav111@users.noreply.github.com>
2022-05-10 23:00:03 +0530

    Add files via upload
    I created a project with the API. Project is to read the online. This project divided three to four parts.Like Angularjs, Recatjs, Javascript etc.

---
+++ app/course/course.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+course= [
+{'id':1,'name' :'Learn Angular','description':'Lorem Ipsum is simply dummy text of the printing and typesetting industry','image':'../../assets/angimg.jpg'},
+{'id':2,'name' :'Learn Typescript','description':'Lorem Ipsum is simply dummy text of the printing and typesetting industry','image':'../../assets/tsimg.jpg'},
+{'id':3,'name' :'Learn Nodejs','description':'Lorem Ipsum is simply dummy text of the printing and typesetting industry','image':'../../assets/nodejsimg.jpg'},
+{'id':4,'name' :'Learn Reactjs','description':'Lorem Ipsum is simply dummy text of the printing and typesetting industry','image':'../../assets/recatjsimg.jpg'}
+
+]
+}
